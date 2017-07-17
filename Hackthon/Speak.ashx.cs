68f0eab0aef82d11e2f9a2a03720dadf1c82d63f
Tianhua Zhao <tianhz@microsoft.com>
2017-07-17 17:46:59 +0800

    check in init project

---
+++ Hackthon/Speak.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackthon
+{
+    /// <summary>
+    /// Summary description for Speak
+    /// </summary>
+    public class Speak : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
